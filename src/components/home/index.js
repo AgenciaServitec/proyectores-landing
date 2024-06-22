d542dbf849195c618573d554486fcd6192d5e359
noel <admin@agenciaservitec.com>
2024-06-22 02:18:24 -0500

    Refactoring home  components and alts

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,3 +1,2 @@
 export { default as CarouselProducts } from "./CarouselProducts.astro";
-export { default as DeliverySection } from "./DeliverySection.astro";
 export { default as AboutUsComponent } from "./AboutUs.astro";
