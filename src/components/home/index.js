fd3f65c0034be9090dacdcfb8d2572f3e431cf42
Ignacio2906 <ignacio.malcer@gmail.com>
2024-06-24 15:38:18 -0500

    Add SeoSection

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,2 +1,3 @@
 export { default as CarouselProducts } from "./CarouselProducts.astro";
 export { default as AboutUsComponent } from "./AboutUs.astro";
+export  {default as SeoSection} from "./SeoSection.astro";
