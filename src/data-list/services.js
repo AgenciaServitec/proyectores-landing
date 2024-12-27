d60b5a54b665ed12b520b5f1031fd1fa0fb8c7a4
Angel Gala <galafloresangelemilio@gmail.com>
2024-12-27 13:06:29 -0500

    Add: added alt attribute to image element and updated services.js

--- src/data-list/services.js
+++ src/data-list/services.js
@@ -1,23 +1,30 @@
 export const services = [
   {
     imagePath: "./images/imagen-borrosa.webp",
+    imageAlt: "Imagen borrosa"
   },
   {
     imagePath: "./images/imagen-con-puntos.webp",
+    imageAlt: "Imagen con puntos"
   },
   {
     imagePath: "./images/imagen-distorsionada.webp",
+    imageAlt: "Imagen distorsionada"
   },
   {
     imagePath: "./images/lampara-no-funciona.webp",
+    imageAlt: "Imagen de lampara no funciona"
   },
   {
     imagePath: "./images/mensaje-sin-senal.webp",
+    imageAlt: "Imagen de mensaje sin señal"
   },
   {
     imagePath: "./images/proyector-sobrecalienta.webp",
+    imageAlt: "Imagen de proyector sobrecalienta"
   },
   {
     imagePath: "./images/se-apaga-y-reinicia.webp",
+    imageAlt: "Imagen de se apaga y reinicia."
   },
 ];
